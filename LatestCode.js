9b4f43545ea3a604847dd3d3a6ace3c29b2b7c64
Atom <121838254+AtomS1101@users.noreply.github.com>
2023-12-15 21:06:07 +0900

    LatestCode.js を更新

--- LatestCode.js
+++ LatestCode.js
@@ -1,1 +1,12 @@
-new code
+//replaced
+const verUrl = "https://raw.githubusercontent.com/AtomS1101/LEAPnewCode_public/main/LatestVersion.txt";
+const codeUrl= "https://raw.githubusercontent.com/AtomS1101/LEAPnewCode_public/main/LatestCode.js";
+const data = new Request(verUrl);
+const latestVer = await data.loadString();
+console.log(latestVer);
+
+const data = new Request(codeUrl);
+const codeString = await data.loadString();
+
+const fm = FileManager.local()
+fm.writeString(module.filename, codeString)
