eb6927f37392bbca0b35d54cb2cc8f9e78345480
Atom <121838254+AtomS1101@users.noreply.github.com>
2023-12-15 22:00:30 +0900

    LatestCode.js を更新

--- LatestCode.js
+++ LatestCode.js
@@ -1,9 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: cyan; icon-glyph: user-check;
-
 //replaced
-new!
 const verUrl = "https://raw.githubusercontent.com/AtomS1101/LEAPnewCode_public/main/LatestVersion.txt";
 const codeUrl= "https://raw.githubusercontent.com/AtomS1101/LEAPnewCode_public/main/LatestCode.js";
 const data = new Request(verUrl);
